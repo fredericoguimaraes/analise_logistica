886fbddaab22dc2853f63c34a8e95051e127841e
Fred <guimaraesp.fred@gmail.com>
2026-07-04 12:04:19 -0300

    analises iniciais sobre a taxa de ineficiencia por dia

--- analise_base.py
+++ analise_base.py
@@ -1,4 +1,5 @@
 import pandas as pd 
 import scipy.stats as stats
 
-base = pd.read_csv('base_chamados.csv', sep=';', encoding='latin-1')
+base = pd.read_csv('base_chamados.csv', sep=',', encoding='latin-1')
+base.describe()
